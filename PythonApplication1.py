a60d4008a184f451618a734a6720612ae28ed166
Nazar <opilane@HP-CZC2349HTC>
2023-12-12 10:43:21 +0200

    1-10

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -39,8 +39,79 @@ from math import *
 #d=round(u/pi,2)
 #print("läbimõõt", d)
 
-N = float(input("Длина участка:"))
-M = float(input("Ширина участка:"))
+#N = float(input("Длина участка:"))
+#M = float(input("Ширина участка:"))
 
-diagonal_lenght = sqrt(N**2 + M**2)
-print(f"Длина диагонали:{diagonal_lenght} метров ")
+#diagonal_lenght = sqrt(N**2 + M**2)
+#print(f"Длина диагонали:{diagonˇal_lenght} метров ")
+
+#6
+#try:
+#    aeg = float(input("Mitu tundi kulus sõiduks? "))
+#    try:
+#       teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+#       kiirus = teepikkus/aeg
+#       print("Sinu kiirus oli " + str(kiirus) + " km/h")
+#    except :
+#       print("Viga andmetaga!")
+
+#except :
+#    print("On vaja ainult numbrid sisestada!")
+
+
+#7 
+#number1 = float(input("Введите любое число:"))
+#number2 = float(input("Введите любое число:"))
+#number3 = float(input("Введите любое число:"))
+#number4 = float(input("Введите любое число:"))
+#number5 = float(input("Введите любое число:"))
+
+#average = (number1 + number2 + number3 + number4 + number5) / 5
+#print(f"Среднее арефметическое: {average}")
+
+#8
+#print("  @..@")
+#print(" (----)")
+#print("( \__/ )")
+#print("^^ \"\" ^^")
+
+#9
+try:
+     a = int(input("Введите сторону А:"))
+     try:
+       b = int(input("Введите сторону B:"))
+       try:
+         c = int(input("Введите сторону С:"))
+         P = a + b + c
+         print(f"Периметр треугольника: {P}")
+       except :
+         print("viga muutujaga!")
+     except :
+      print("viga muutujaga!")
+except :
+    print("On vaja täisarv kasutada")
+
+try:
+    c = int(input("Введите сторону С:"))
+except :
+    print("viga muutujaga!")
+
+
+
+
+#10
+#try:
+#    P=int(input("sõbrade kogus:"))
+#except :
+#    pass
+#Pitsa_hind= 12.90
+
+
+#Protsenti = 10
+
+#jootraha = (Protsenti / 100) * Pitsa_hind
+
+#kokku_maksta = Pitsa_hind + jootraha
+
+#kokku = kokku_maksta / P
+#print(f"kokku maksta: {kokku:.2f} euro")
